865ceda4164bb909e3b282d56c473052aed57800
SordoAriel <a.a.sordo@gmail.com>
2023-10-27 19:40:24 -0300

    Desafío entregable Implementación de Login

--- src/router/users.router.js
+++ src/router/users.router.js
@@ -3,6 +3,12 @@ import { usersManager } from "../dao/DB/Managers/usersManager.js";
 
 const router = Router()
 
+router.get("/logout", async (req, res) =>{
+    req.session.destroy(() =>{
+        res.redirect("http://localhost:8080/login")
+    })
+})
+
 router.post("/signup", async (req, res)=>{
     const newUser = await usersManager.add(req.body)
     res.status(200).json({message: 'Usuario creado', newUser})
